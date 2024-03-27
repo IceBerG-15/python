221697179f9b6fe4a59b8560c16c56e6c17f9eaa
IceBerG-15 <warmachiness13@gmail.com>
2024-03-27 22:01:21 +0530

    updates

--- day-64/main.py
+++ day-64/main.py
@@ -11,6 +11,14 @@ from dotenv import load_dotenv
 import os
 # loading dotenv file
 load_dotenv('day-64/file.env')
+TMDB = os.getenv('API_KEY')   # getting the api key
+TMBD_BEARER = f'Bearer {os.getenv('TMBD_BEARER')}'
+TMBD_URL = 'https://api.themoviedb.org/3/search/movie/'
+TMDB_IMG_URL = 'https://image.tmdb.org/t/p/original/'
+headers = {
+        "accept": "application/json",
+        "Authorization": TMBD_BEARER
+        }
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key'
@@ -30,9 +38,9 @@ class Movie(db.Model):
     title:  Mapped[str] = mapped_column(String, nullable=False, unique=True)
     year:   Mapped[str] = mapped_column(String,nullable=False)
     desc:   Mapped[str] = mapped_column(String, nullable=False)
-    rating: Mapped[float] = mapped_column(Float, nullable=False)
-    ranking:Mapped[int] = mapped_column(Integer, nullable=False, unique=True)
-    review: Mapped[str] = mapped_column(String, nullable=False)
+    rating: Mapped[float] = mapped_column(Float,nullable=True)
+    ranking:Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String, nullable=True)
     img_url:Mapped[str] = mapped_column(String, nullable=False)
 
 with app.app_context():
@@ -41,16 +49,17 @@ with app.app_context():
 # -----------------------------------------------------
 # adding a new movie
 # with app.app_context(): 
-#     # new_movie = Movie(
-#     #     title="Phone Booth",
-#     #     year=2002,
-#     #     desc="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     #     rating=7.3,
-#     #     ranking=10,
-#     #     review="My favourite character was the caller.",
-#     #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-#     # )
-#     # db.session.add(new_movie)
+#     new_movie = Movie(
+#         title="Phone Booth",
+#         year=2002,
+#         desc="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller.",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+#     db.session.add(new_movie)
+#     db.session.commit()
 #     second_movie = Movie(
 #         title="Avatar The Way of Water",
 #         year=2022,
@@ -72,7 +81,7 @@ class ratingForm(FlaskForm):
 
 # adding movie form
 class addingForm(FlaskForm):
-    title = StringField('Enter movie title',validators=DataRequired())
+    title = StringField('Enter movie title')
     submit = SubmitField('Submit')
 
 # home route
@@ -105,15 +114,32 @@ def delete():
     return redirect(url_for('home'))
 
 # movie add route
-@app.route("/add",methods=["GET", "POST"])
+@app.route("/add",methods=["GET", "POST"])   #,dnsNameservers= '8.8.8.8'
 def add():
     form = addingForm()
-    # if form.validate_on_submit():
-    #     db.session.add()
-    #     db.session.commit()
-    #     return redirect(url_for('home'))
+    if form.validate_on_submit():
+        response = requests.get(url=TMBD_URL,params={'query':form.title.data},headers=headers)
+        results = response.json()['results']
+        return render_template('select.html', movies = results)
     return render_template("add.html",form=form)
 
+# finding movie
+@app.route("/find",methods=["GET", "POST"])
+def find():
+    id = request.args.get('id')
+    url = TMBD_URL+f'{id}'
+    response = requests.get(url=url,headers=headers)
+    data = response.json()
+    movie = Movie(
+        id = id,
+        title = data['title'],
+        year = data['release_date'].split('-')[0],
+        desc = data['overview'],
+        img_url = f'{TMDB_IMG_URL}{data['poster_path']}'
+    )
+    db.session.add(movie)
+    db.session.commit()
+    return redirect(url_for('edit',id=id))
 
 if __name__ == '__main__':
     app.run(debug=True)
